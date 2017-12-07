2335544dcc311b32c6b70c21c643116096018673
LawLawL <nbaspin@gmail.com>
2017-12-07 19:10:51 -0400

    ajout du support de plusieurs langues

--- capt.sh
+++ capt.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+choix=""
+
+while [ "$choix" != "1" ] && [ "$choix" != "2" ] && [ "$choix" != "3" ] && [ "$choix" != "4" ] && [ "$choix" != "5" ]
+do
+    #echo "Entrez 'fr' pour télécharger la version française ou 'de' pour la version allemande:"
+	echo "---------------------------------------------------"
+	echo "Entrez le numéro correspondant à la version que vous voulez télécharger:"
+	echo "1 pour la version française"
+	echo "2 pour la version allemande"
+	echo "3 pour la version espagnole"
+	echo "4 pour la version anglaise"
+	echo "5 pour la version polonaise"
+    read -p "Votre choix:" choix
+done
+
+
 currentdir=$(pwd)
 
 cd /tmp
@@ -18,17 +34,6 @@ LINE=$(echo $ligne2 | tr ' ' '\n' | grep -A 2 "src" | head -n 1  | cut -d '"' -f
 
 test_curl=`curl -Is "$LINE" | head -1 | cut -f 2 -d " "` #test si le player est accessible
 
-choix=""
-
-while [ "$choix" != "fr" ] && [ "$choix" != "de" ]
-do
-    #echo "Entrez 'fr' pour télécharger la version française ou 'de' pour la version allemande:"
-	echo "---------------------------------------------------"
-    read -p "Entrez 'fr' pour télécharger la version française ou 'de' pour la version allemande:" choix
-done
-
-
-
 if [ "$test_curl" = "200" ];then
 	#echo "---------------------------------------------------"
 	#echo "Line:" $LINE
@@ -38,21 +43,31 @@ if [ "$test_curl" = "200" ];then
 	echo "---------------------------------------------------"
 	url_dec=$(printf "${url_enc//%/\\x}")	#enlève l'encodage html
 	
-	if [ "$choix" = "fr" ];then
-	    #echo "Français choisi"
-        url_dec=$(echo "$url_dec" | python -c "import sys; tmp = sys.stdin.readline().split('/'); tmp[7] = 'fr';print '/'.join(tmp)")
+	if [ "$choix" = "1" ];then
+        langue='fr'
+	elif [ "$choix" = "2" ];then
+	    langue='de'
+	elif [ "$choix" = "3" ];then
+	    langue='es'
+	elif [ "$choix" = "4" ];then
+	    langue='en'
+	elif [ "$choix" = "5" ];then
+	    langue='pl'
 	fi
 	
-	if [ "$choix" = "de" ];then
-	    #echo "Allemand choisi"
-        url_dec=$(echo "$url_dec" | python -c "import sys; tmp = sys.stdin.readline().split('/'); tmp[7] = 'de';print '/'.join(tmp)")
-	fi 
+    url_dec=$(echo "$url_dec" | python -c "import sys; tmp = sys.stdin.readline().split('/'); tmp[7] = '$langue';print '/'.join(tmp)")
+	
+	test_langue=`curl -Is "$url_dec" | head -1 | cut -f 2 -d " "`
+	
+	if [ ! "$test_langue" = "200" ];then
+	    echo "La langue demandée ne semble pas disponible, fin du programme"
+	fi
 	
 	echo "Url du fichier de configuration de l'api:" $url_dec
 	wget --header="Accept: text/html" --user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:21.0) Gecko/20100101 Firefox/21.0" -q -O api_page $url_dec #télécharge l'api
   
 else
-    echo "Probleme avec l'url, fin du programme"
+    echo "Le player semble inaccessible, fin du programme"
 	exit
 fi
 
@@ -60,19 +75,21 @@ fi
 #cat api_page | grep -w -A 7 "HTTP_MP4_SQ_1" | sed 's/"/\n/g' | sed 's/[\]//g' > fichier_frag_2
 cat api_page | grep -w -A 7 "HTTPS_SQ_1" | sed 's/"/\n/g' | sed 's/[\]//g' > fichier_frag_2 # télécharge la version en meilleure qualité 
 
-title1=$(echo -en "$(cat api_page | grep "VTI" | cut -d '"' -f 4 | tr -d ':' | tr -d '?')")
-title0=$(echo -en "$(cat api_page | grep "subtitle" | cut -d '"' -f 4 | tr -d ':' | tr -d '?')")
+title0=$(echo -en "$(cat api_page | grep "VTI" | cut -d '"' -f 4 | tr -d ':' | tr -d '?')") #le VTI est toujours présent dans le code
+title1=$(echo -en "$(cat api_page | grep "subtitle" | cut -d '"' -f 4 | tr -d ':' | tr -d '?')") #mais pas toujours le subtitle
 
-if [ ! -z "$subtitle" ]; then
+if [ ! -z "$title1" ]; then #si $title1 n'est pas vide, alors on l'utilise
     echo "---------------------------------------------------"
     echo "Nom du programme:" $title0
     echo "---------------------------------------------------"
     echo "Nom du podcast:" $title1
-else 
+else #sinon, just title 0
     echo "---------------------------------------------------"
     echo "Nom du podcast:" $title0
 fi
 
+echo "---------------------------------------------------"
+echo "Cherche le lien du fichier de configuration de l'api (veuillez patienter quelques secondes)..."
 
 
 cat fichier_frag_2 | while read LINE
@@ -81,7 +98,7 @@ cat fichier_frag_2 | while read LINE
 
 do
 #echo "This is a line:" $LINE
-    test_curl=`curl -Is "$LINE" | head -1 | cut -f 2 -d " "`
+    test_curl=`curl -Is "$LINE" | head -1 | cut -f 2 -d " "` #on vérifie quelle ligne est un lien 
 
 #echo "Test line:" $test_curl
 
@@ -103,15 +120,16 @@ rm -R work
 url_frag=(${url//*:\/\// })
 url_var=${url_frag[0]}
 
-echo "url var:" $url_var
-
+#echo "url var:" $url_var
+echo "Téléchargement du fichier:"
+echo 
+echo
+echo
 #echo "test:$title .mp4"
+cd "$currentdir"
 
-if [ ! -z "$subtitle" ]; then
-    wget -O "$title0 - $title1 .mp4" $url_var
-	mv "$title0 - $title1 .mp4" "$currentdir"
+if [ ! -z "$title1" ]; then
+    wget -O "$title0 - $title1 [Arte].mp4" $url_var
 else 
-    wget -O "$title1 .mp4" $url_var
-	mv "$title1 .mp4" "$currentdir"
+    wget -O "$title0 [Arte].mp4" $url_var
 fi
-
